df08539d6735b0c80ebb3aee52e96e923e91d0a6
Igor Koshelev <igor.koshelev440@gmail.com>
2021-03-04 19:12:24 +0300

    fix 4
    изменил дефолтную тему на темную

--- src/application/view.ts
+++ src/application/view.ts
@@ -1,25 +1,46 @@
 import { XMessage } from '../messages';
 import { SlideTheme } from './types';
 
+export type ExtendedIframeElement = HTMLIFrameElement & {
+  _isLoaded: boolean
+}
+
 export const setScale = (el: HTMLDivElement, value: number) => {
     el.style.transform = `scaleX(${value.toFixed(5)})`;
 }
 
-export const sendMessage = (iframe: HTMLIFrameElement, msg: XMessage) => {
-    if (iframe && iframe.contentWindow) {
-        iframe.contentWindow.postMessage(msg, '*');
+export const sendMessage = (iframe: ExtendedIframeElement, msg: XMessage) => {
+  if (iframe && iframe.contentWindow) {
+    if (iframe._isLoaded) {
+      iframe.contentWindow.postMessage(msg, "*");
+    } else {
+      iframe.addEventListener(
+        "load",
+        () => {
+          iframe.contentWindow.postMessage(msg, "*");
+          iframe._isLoaded = true
+        },
+        {
+          once: true,
+        }
+      );
     }
-}
+  }
+};
 
-export const initIframe = (parent: HTMLDivElement, onLoad: (iframe: HTMLIFrameElement) => void) => {
-    var iframe = document.createElement('iframe');
+export const initIframe = (parent: HTMLDivElement, onLoad: (iframe: ExtendedIframeElement) => void) => {
+    var iframe = document.createElement('iframe') as ExtendedIframeElement;
+    iframe._isLoaded = false
 
     iframe.classList.add('frame');
     iframe.src = 'frame.html';
     iframe.sandbox.add('allow-scripts');
     iframe.sandbox.add('allow-same-origin');
 
-    iframe.addEventListener('load', () => onLoad(iframe));
+    iframe.addEventListener('load', () => {
+      onLoad(iframe)
+      iframe._isLoaded = true
+    });
     parent.appendChild(iframe);
 
     return iframe;
